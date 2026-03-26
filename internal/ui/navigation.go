fa4beab55cbdac85cbfd2885370e9b3de5e41094
Sameer Magan <sm.sameermagan@gmail.com>
2026-03-26 11:05:00 +0000

    feat: rewrite-flutter

--- internal/ui/navigation.go
+++
@@ -1,5 +0,0 @@
-package ui
-
-// This file previously contained CreateNavigationBar which is now deprecated
-// The navigation functionality is now handled by fyne's built-in container.Navigation
-// This file is kept for potential future navigation-related utilities
